758a8fe2d54f8971d5c2d2975e3beb799d352e87
omar-fi <filaliomar070@gmail.com>
2025-12-17 23:29:41 +0100

    first

--- src/main/java/org/example/tp_rest/controlleur/UserController.java
+++ src/main/java/org/example/tp_rest/controlleur/UserController.java
@@ -5,7 +5,7 @@ import org.example.tp_rest.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.awt.*;
+
 import java.util.List;
 
 @RestController
@@ -29,12 +29,12 @@ public class UserController
     }
 
     @PutMapping("/update/{p}")
-    public String UpdateUser(@PathVariable("p") int id,@RequestBody User user){
+    public String updateUser(@PathVariable("p") int id,@RequestBody User user){
         return userService.updateUser(id,user);
     }
 
     @DeleteMapping("/supp")
-    public void SupprimerUser(@RequestParam("p")
+    public void supprimerUser(@RequestParam("p")
                               Integer id){
         userService.suppUser(id);
     }
